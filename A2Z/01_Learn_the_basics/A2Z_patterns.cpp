9897a9156618c6286aa8bd62bb586f9ba0b632c3
hemant11ks <ksingh1617@gmail.com>
2026-02-25 13:46:06 +0530

    Adding half diamond expressions

--- A2Z/01_Learn_the_basics/A2Z_patterns.cpp
+++ A2Z/01_Learn_the_basics/A2Z_patterns.cpp
@@ -56,9 +56,115 @@ void rightAngleTrianglPatternReverseAnother(int n)
 void halfUpwardPyramid(int n)
 {
    // First loop of space, Second Loop for star and third loop for space
-   
+   for(int i = 0; i<n; i++){
+      // First space
+      for(int j = 0; j<n-i-1; j++){
+          cout<<"  ";
+      }
+      // Mid star
+      for(int k = 0; k<2*i + 1; k++){
+          cout<<"* ";
+      }
+      // Last space
+      for(int l = 0; l<n-i-1; l++){
+          cout<<"  ";
+      }
+      cout<<"\n";
+   }
 
 }
+
+void halfDownwardPyramid(int n)
+{
+   // First loop of space, Second Loop for star and third loop for space
+   for(int i = 0; i<n; i++){
+      // First space
+      for(int j = 0; j<i; j++){
+          cout<<"  ";
+      }
+      // Mid star
+      for(int k = 0; k<2*n - (2*i + 1); k++){
+          cout<<"* ";
+      }
+      // Last space
+      for(int l = 0; l<i; l++){
+          cout<<"  ";
+      }
+      cout<<"\n";
+   }
+
+}
+
+
+void diamondPattern(int n)
+{
+   // First loop of space, Second Loop for star and third loop for space
+   for(int i = 0; i<n; i++){
+      // First space
+      for(int j = 0; j<n-i-1; j++){
+          cout<<"  ";
+      }
+      // Mid star
+      for(int k = 0; k<2*i + 1; k++){
+          cout<<"* ";
+      }
+      // Last space
+      for(int l = 0; l<n-i-1; l++){
+          cout<<"  ";
+      }
+      cout<<"\n";
+   }
+
+    // First loop of space, Second Loop for star and third loop for space
+   for(int i = 0; i<n; i++){
+      // First space
+      for(int j = 0; j<i; j++){
+          cout<<"  ";
+      }
+      // Mid star
+      for(int k = 0; k<2*n - (2*i + 1); k++){
+          cout<<"* ";
+      }
+      // Last space
+      for(int l = 0; l<i; l++){
+          cout<<"  ";
+      }
+      cout<<"\n";
+   }
+
+}
+
+void diamondHalfPattern(int n){
+   for(int i=0; i<n; i++){
+      // star
+      for(int j=0; j<i; j++){
+          cout<<"*";
+      }
+      // space
+      for(int k=0; k<n-i-1; k++){
+          cout<<" ";
+      }
+
+      cout<<"\n";
+
+   }
+   
+   for(int i=0; i<n; i++){
+      // star
+      for(int j=0; j<n-i-1; j++){
+          cout<<"*";
+      }
+      // space
+      for(int k=0; k<i; k++){
+          cout<<" ";
+      }
+
+      cout<<"\n";
+
+   }
+}
+
+
 int main()
 {
     // Adding the logic to run in testcases
@@ -74,7 +180,10 @@ int main()
     // rightAngleTrianglPatternSameI(5);
     // rightAngleTrianglPatternReverse(5);
     // rightAngleTrianglPatternReverseAnother(5);
-    halfUpwardPyramid(5);
+    // halfUpwardPyramid(5);
+    // halfDownwardPyramid(5);
+    // diamondPattern(5);
+    diamondHalfPattern(5);
 
  return 0;
 }
