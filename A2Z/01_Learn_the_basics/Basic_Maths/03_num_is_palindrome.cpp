c03d217e64df29bbb4aa7001b349b1ca99fadbe4
hemant11ks <ksingh1617@gmail.com>
2026-03-03 22:47:55 +0530

    Add the number is palindrome or not code

---
+++ A2Z/01_Learn_the_basics/Basic_Maths/03_num_is_palindrome.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool palindrome(int number) {
+    int revNum = 0; // Initialize a variable to store the reverse of the number
+    int dup = n; // Create a duplicate variable to store the original number
+
+    // Iterate through each digit of the number until it becomes 0
+    while (n > 0) {
+        int ld = n % 10; // Extract the last digit of the number
+        revNum = (revNum * 10) + ld; // Build the reverse number by appending the last digit
+        n = n / 10; // Remove the last digit from the original number
+    }
+
+    // Check if the original number is equal to its reverse
+    if (dup == revNum) {
+        return true; // If equal, return true indicating it's a palindrome
+    } else {
+        return false; // If not equal, return false indicating it's not a palindrome
+    }
+}
+
+int main()
+{
+  int number = 4554;
+
+  if(palindrome(number)){
+    cout<<number<<" is a palindrome. "<<endl;
+  } else {
+    cout<<number<<" is not a palindrome. "<<endl;
+  }
+
+ return 0;
+}
